4e3e629a3bb61d881ff57434fe68347f0a1a9c88
hOtoch <hugootoch@hotmail.com>
2025-08-04 14:23:56 -0300

    build: Resolve o erro da notação '@'

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,4 +9,9 @@ export default defineConfig({
     outDir: "dist",
     manifest: true,     // opcional, mas futuro-proof
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 });
